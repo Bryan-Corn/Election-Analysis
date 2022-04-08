4a2244d2782e41887c6ec7a89296a608db042e1c
unknown <bryan.corn@gmail.com>
2022-04-08 12:48:24 -0700

    Adding the election audit code.

---
+++ grocerylist.py
@@ -0,0 +1,6 @@
+grocery_list = ["Milk", "Bread", "Eggs", "Peanut Butter", "Jelly"]
+print(grocery_list)
+grocery_list[3] = "Almond Butter"
+grocery_list.remove("Jelly")
+grocery_list.append("Coffee")
+print(grocery_list)
